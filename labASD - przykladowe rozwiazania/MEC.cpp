1b10240e8dc5bfe9a5e040b2a07eb177daefae91
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2017-03-10 16:19:29 +0100

    komentarze do MEC

--- labASD - przykladowe rozwiazania/MEC.cpp
+++ labASD - przykladowe rozwiazania/MEC.cpp
@@ -1,3 +1,4 @@
+// Michal Naruniec
 #include<cstdio>
 #include<vector>
 #include<algorithm>
@@ -7,14 +8,21 @@ using namespace std;
 int main() {
   int n, m, input;
   scanf("%d %d", &n, &m);
+  
+  // Inicjalizacja dwuwymariowego wektora booli o rozmiarze n x m.
   vector<vector<bool> > masks(n, vector<bool> (m)); 
   for(int i = 0; i < m; i++) {
     for(int j = 1; j <= n; j++) {
       scanf("%d", &input);
+      
+      // Przypisanie do druzyn - true to gra w druzynie 1.
       masks[input - 1][i] = (j <= (n / 2));
     }
   }
+  // Sortowanie wektorow po domyslnym komparatorze.
   sort(masks.begin(), masks.end());
+  
+  // Funkcja znajdujaca 2 sasiednie, rowne wiersze.
   printf(adjacent_find(masks.begin(), masks.end()) == masks.end() ? "TAK\n" : "NIE\n");
   return 0;
 }
